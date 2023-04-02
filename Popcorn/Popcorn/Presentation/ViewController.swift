86f7699feae50ab7e2d936e9b37f958c775d01ee
Hoang Nguyen <hoang21091989@gmail.com>
2023-04-02 12:30:33 +0800

    Init project

---
+++ Popcorn/Popcorn/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Popcorn
+//
+//  Created by Hoang Nguyen on 2/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ Popcorn/PopcornTests/PopcornTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornTests.swift
+//  PopcornTests
+//
+//  Created by Hoang Nguyen on 2/4/23.
+//
+
+import XCTest
+@testable import Popcorn
+
+final class PopcornTests: XCTestCase {
+
+    func test_fail() {
+        
+    }
+
+}
